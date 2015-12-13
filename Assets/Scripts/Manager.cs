cf00c5b1c16e1e420b25af3e242998201de0c990
YIvan <megavirus74@gmail.com>
2015-12-13 18:08:15 +0500

    Manager fix

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,37 +1,35 @@
-﻿using System;
-using System.Runtime.Remoting.Messaging;
-using UnityEngine;
-using System.Collections;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class Manager : MonoBehaviour
-{
-    private float score;
-    private float time;
-    private int stage;
-
+public class Manager : MonoBehaviour {
     public enum GameState {
-        MainMenu, Playing, GameOver, PreparingUI
+        MainMenu,
+        Playing,
+        GameOver,
+        PreparingUI
     }
 
-    private float menuElementsSpeed = 0.03f;
-
-    public GameObject playerGameObject;
-    public GameObject mainMenuGameObject;
-    public GameObject gameBackgroundObject;
     public GameObject arrowObject;
+    public GameObject gameBackgroundObject;
     public GameState gameState;
 
     public Text gameTimeText;
+    public GameObject mainMenuGameObject;
+    private float menuElementsSpeed = 0.03f;
+
+    public GameObject playerGameObject;
+    private float score;
+    private int stage;
+    private float time;
     // Use this for initialization
 
-    void Start() {
+    private void Start() {
         ColorPalette.InitPalleteNum(1);
         Camera.main.backgroundColor = ColorPalette.background1Color;
         gameBackgroundObject.GetComponent<SpriteRenderer>().color = ColorPalette.background2Color;
         mainMenuGameObject.GetComponent<SpriteRenderer>().color = ColorPalette.background2Color;
         arrowObject.GetComponent<SpriteRenderer>().color = ColorPalette.background1Color;
-        
+
         gameState = GameState.MainMenu;
         mainMenuGameObject.transform.position = new Vector3(0, 0, 1);
         gameBackgroundObject.transform.position = new Vector3(0,
@@ -55,21 +53,21 @@ public class Manager : MonoBehaviour
     //}
 
     // Update is called once per frame
-    private void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.UpArrow) && gameState == GameState.MainMenu)
-        {
-        if (gameState == GameState.PreparingUI)
-        {
+    private void Update() {
+        if (Input.GetKeyUp(KeyCode.UpArrow) && gameState == GameState.MainMenu) {
+            gameState = GameState.PreparingUI;
+        }
+
+        if (gameState == GameState.PreparingUI) {
             if (gameBackgroundObject.transform.position.y > 0) {
                 float yPosChange = menuElementsSpeed*(gameBackgroundObject.transform.position.y + 1);
                 gameBackgroundObject.transform.position -= new Vector3(0, yPosChange, 0);
                 mainMenuGameObject.transform.position -= new Vector3(0, yPosChange, 0);
+            }
+            else {
                 gameState = GameState.Playing;
             }
         }
     }
 
-
 }
-
